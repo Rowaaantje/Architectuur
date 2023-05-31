2495bf47271e4a141e655800c0500565dbea2070
Rowaaantje <114189377+Rowaaantje@users.noreply.github.com>
2023-05-31 12:10:14 +0200

    Player Movement Concept
    3/4 finished

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -6,30 +6,69 @@ public class player : MonoBehaviour
 {
     float playerHeight = 2f;
 
+    [SerializeField] Transform orientation;
+
     [Header("Movement")]
     public float moveSpeed = 6f;
     [SerializeField] float airMultiplier = 0.4f;
     float movementMultiplier = 10f;
+
+    [Header("Sprinting")]
+    [SerializeField] float walkSpeed = 4f;
+//    [SerializeField] float sprintSpeed = 6f;
+    [SerializeField] float acceleration = 18f;
+
+    [Header("Camera")]
+    [SerializeField] private Camera cam;
+    [SerializeField] private float fov;
+    [SerializeField] private float runFov;
+    [SerializeField] private float runFovTime;
 
     [Header("Jumping")]
     public float jumpForce = 5f;
 
     [Header("Keybinds")]
     [SerializeField] KeyCode jumpKey = KeyCode.Space;
+    [SerializeField] KeyCode sprintKey = KeyCode.LeftShift;
 
     [Header("Drag")]
     [SerializeField] float groundDrag = 6f;
     [SerializeField] float airDrag = 2f;
 
-
     float horizontalMovement;
     float verticalMovement;
 
+    [Header("Ground Detection")]
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask groundMask;
+    [SerializeField] float groundDistance = 0.2f;
     bool isGrounded;
 
     Vector3 moveDirection;
+    Vector3 slopeMoveDirection;
+
+    [Header("Gravity")]
+    [SerializeField] public float gravity;
 
     Rigidbody rb;
+
+    RaycastHit slopeHit;
+
+    private bool OnSlope()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight / 2 + 0.5f))
+        {
+            if (slopeHit.normal != Vector3.up)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return false;
+    }
 
     private void Start()
     {
@@ -39,15 +78,19 @@ public class player : MonoBehaviour
 
     private void Update()
     {
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight / 2 + 0.1f);
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         MyInput();
         ControlDrag();
+        ControlSpeed();
+        Gravity();
 
         if (Input.GetKeyDown(jumpKey) && isGrounded)
         {
             Jump();
         }
+
+        slopeMoveDirection = Vector3.ProjectOnPlane(moveDirection, slopeHit.normal);
     }
 
     void MyInput()
@@ -55,13 +98,35 @@ public class player : MonoBehaviour
         horizontalMovement = Input.GetAxisRaw("Horizontal");
         verticalMovement = Input.GetAxisRaw("Vertical");
 
-        moveDirection = transform.forward * verticalMovement + transform.right * horizontalMovement; //using Transform.right & forward so we move in the direction relitive where the player is looking
+        moveDirection = orientation.forward * verticalMovement + orientation.right * horizontalMovement; //using Transform.right & forward so we move in the direction relitive where the player is looking
     }
 
     void Jump()
-    {
-        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    {   
+        if(isGrounded)
+        {               
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        }
     }
+
+    void ControlSpeed()
+    {
+        float f = fov; // rik
+        if (Input.GetKey(sprintKey) && isGrounded && rb.velocity.magnitude > 0.0f)
+        {
+            f = runFov; //rik
+        }
+
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, f, runFovTime * Time.deltaTime);
+        moveSpeed = Mathf.Lerp(moveSpeed, walkSpeed, acceleration * Time.deltaTime);
+    }
+
+    void Gravity()
+    {
+        rb.AddForce(0, -gravity, 0);
+    }
+
 
     void ControlDrag()
     {
@@ -82,13 +147,17 @@ public class player : MonoBehaviour
 
     void MovePlayer()
     {
-        if (isGrounded)
+        if (isGrounded && !OnSlope())
         {
             rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration); //normalized makes it so if you move vertical (W+A || W+D ) you wont move faster  than just W || S
         }
+        else if (isGrounded && OnSlope())
+        {
+            rb.AddForce(slopeMoveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
+        }
         else if (!isGrounded)
         {
-            rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier * airMultiplier, ForceMode.Acceleration);  
+            rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier * airMultiplier, ForceMode.Acceleration);
         }
     }
 }
--- Assets/Scripts/playerLook.cs
+++ Assets/Scripts/playerLook.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,36 +8,25 @@ public class playerLook : MonoBehaviour
     [SerializeField] private float sensY;
 
 
-    Camera cam;
+    [SerializeField] Transform cam; //we want to reference the camera holder in the inspector
+    
 
+     [SerializeField] Transform orientation;
     float mouseX;
-
     float mouseY;
 
 
     float multiplier = 0.01f;
-
     float xRotation;
     float yRotation;
 
-
     private void Start()
     {
-        cam = GetComponentInChildren<Camera>();
-
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
     private void Update()
-    {
-        MyInput();
-
-        cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);  //for the camera we want to set the local rotation to rotate by oure x ratation on the x-axis 
-        transform.rotation = Quaternion.Euler(0, yRotation,  0); //only rotate the player on the y-axis
-    }
-
-    void MyInput()
     {
         mouseX = Input.GetAxisRaw("Mouse X");
         mouseY = Input.GetAxisRaw("Mouse Y");
@@ -48,8 +37,7 @@ public class playerLook : MonoBehaviour
 
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-
+        cam.transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0);  //for the camera we want to set the local rotation to rotate by oure x ratation on the x-axis 
+        orientation.transform.rotation = Quaternion.Euler(0, yRotation,  0); //only rotate the player on the y-axis
     }
-    
-
 }
